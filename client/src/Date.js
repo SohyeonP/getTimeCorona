2e42a5858ff1da742dc4dca14c5fb189ef5888e3
SohyeonPark <hidhen9714@naver.com>
2020-09-17 16:56:45 +0900

    change server& upgrade11

--- client/src/Date.js
+++ client/src/Date.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-function Date(){
-const now = new Date();
-   now.toDateString().slice(0,10);
+function Date() {
+    const now = new Date();
+    now.toDateString().slice(0, 10);
 
-return now;
+    return now;
 }
+
 export default Date;
--- server/src/main/java/com/alpha/dom/DomApplication.java
+++ server/src/main/java/com/alpha/dom/DomApplication.java
@@ -1,13 +1,13 @@
-package com.alpha.corona19.demo;
+package com.alpha.dom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DomApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DomApplication.class, args);
     }
 
 }

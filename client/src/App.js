8521c3c281d22268e13428a1ab7ebae5249e10b3
ramram419 <barbara0419@naver.com>
2020-09-17 17:52:06 +0900

    commit 이여!

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import img1 from '../src/images/search.png';
 import './App.css';
-import logo from '../src/images/logo.png';
+import Logo from './logo';
+// import logo from '../src/images/logo.png';
 
 
 function App() {
-  const [message,setMessage] = useState("");
+  const [message, setMessage] = useState("");
   useEffect(() => {
     fetch('/api/hello')
       .then(response => response.text())
@@ -17,33 +18,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-        <div class="logo"><img src={logo} alt=""></img><span class="update-date">업데이트 날짜- 2020 .09.14</span></div>
+        <Logo />
         <div className="App-title">{message}</div>
       </header>
-      <body>
 
-        <div class="search-bar"><input placeholder="코로나 내용을 검색해 주세요" /><img src={img1} alt="" /></div>
-        <h3>오늘의 확진자</h3>
-        <div class="content-box">
+      <div className="search-bar"><input placeholder="코로나 내용을 검색해 주세요" /><img src={img1} alt="" /></div>
+      
+      <div className="menu">
+        <div>Menu</div>
+      </div>
+      
+     
+      <div className="content-box">
 
-          <div class="clearfix1">
-            <div><h4>확진자 그래프</h4></div>
-            <div class="box" id="chart1"> content box1</div>
-            <div class="box"> <canvas></canvas></div>
-            <div class="box"> <canvas></canvas></div>
-          </div>
-
-
-
-          <div class="clearfix2">
-            <div><h4>코로나 주요 뉴스</h4></div>
-            <div class="box"> News box1</div>
-            <div class="box"> News box2</div>
-            <div class="box"> News box3</div>
-          </div>
+        <div className="clearfix1">
+          <div><h4>확진자 그래프</h4></div>
+          <div className="box" id="chart1"> content box1</div>
+          <div className="box"> <canvas></canvas></div>
+          <div className="box"> <canvas></canvas></div>
         </div>
-      </body>
+
+
+
+        <div className="clearfix2">
+          <div><h4>코로나 주요 뉴스</h4></div>
+          <div className="box"> News box1</div>
+          <div className="box"> News box2</div>
+          <div className="box"> News box3</div>
+        </div>
+      </div>
     </div>
   );
 }

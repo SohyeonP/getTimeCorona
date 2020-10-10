9dbb326a91008e90ffd47b3cc5e5ee79292986c3
barbara0419 <barbara0419@naver.com>
2020-10-10 17:04:20 +0900

    contact 페이지 완료

--- server/frontend/src/Mail.js
+++ server/frontend/src/Mail.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import ReactContactForm from 'react-mail-form';
 import './css/Mail.css';
+import { Link } from "react-router-dom";
+import $ from 'jquery';
 
-class Mail extends React.Component {	
+$(document).ready(function() {
+  $(".send").click(function() {
+			$(".title").css("display","none");
+    })
+  })
 
+
+
+class Mail extends React.Component {
 	render() {
 		return (
-			<div className="contact">
-				<h1>CONTACT</h1>
-			<ReactContactForm className="form" to="rladkfka0419@gmail.com"
-				titleMaxLength={500}
-				contentsMaxLength={10000}
-				titlePlaceholder="답변 받을 이메일을 입력해주세요"
-				contentsPlaceholder="내용을 입력해주세요"
-				buttonText="보내기"
-			/>
+			<div id="contact">
+				<h1 className="title">CONTACT</h1>
+				<form className="gform" method="POST"
+					action="https://script.google.com/macros/s/AKfycbyr-kxuOE7g4I73tF_m9zBHZDtiKMXpluCyswyUTgiU_7N1-ybL/exec"
+				>
+					{<div className="form-elements">
+						<input type="email"
+							placeholder="답변 받을 메일 주소를 입력해주세요."
+							name="email"
+						/>
+						<textarea type="text"
+							placeholder="내용을 입력해주세요"
+							name="message"
+						/>
+						<button className="send">보내기</button>
+					</div>}
+					<div className="thankyou_message"
+						style={{ display: 'none' }}
+					>
+						<h2>메일을 발송하였습니다.<br />빠른 시일내에 답변 드리겠습니다.</h2>
+						<Link to="/">Home</Link>
+					</div>
+				</form>
 			</div>
 		)
 	}
